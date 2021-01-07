4d5ab78055e0ae9e6389d92c34c572b5d0dca8a3
Sandro Putraa <59155826+sandrocods@users.noreply.github.com>
2021-01-07 10:27:05 +0700

    Update Class_tiktok.php

--- lib/Class_tiktok.php
+++ lib/Class_tiktok.php
@@ -131,11 +131,53 @@ class TiktokDownloader
             ],
             null
             );
-            $filename = "./tmp/" . time() . ".mp4";
+            $filename = ".\\TiktokClass\\tmp\\" . $VideoID . ".mp4";
             $d = fopen($filename, "w");
-            fwrite($d, $curl['Body']);
+            $fwrite = fwrite($d, $curl['Body']);
             fclose($d);
-            return $filename;
+            if($fwrite === FALSE){
+                return [
+                    'Status' => false,
+                    'Data' => 'Failed Writing File'
+                ];
+            }else{
+                return [
+                    'Status' => true,
+                    'Data' => 'Success Writing File',
+                    'FilePath' => $filename
+                ];
+
+            }
+            //
+            
         
+    }
+
+    public function RandomVideo($username = null){
+        $Random = TiktokDownloader::curl(
+            'https://www.tiktok.com/@'. $username,
+            'GET',
+            null,
+            [
+                'Max' => '2'
+            ],
+            [
+                'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+            ],
+            null
+        );
+        if (strpos($Random['Body'], '"serverCode":200')) {
+            preg_match_all('/video":{"id":"(.*?)"/',$Random['Body'], $out_vid);
+            return [
+                'Status' => true,
+                'VideoID' => $out_vid[1]
+            ];
+        }else{
+            return [
+                'Status' => false,
+                'Data' => 'Error Username not available / Another error'
+            ];
+        }
     }
 }
