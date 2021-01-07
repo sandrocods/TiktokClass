03f878d59547aefb0f6bdabc387e73c11c43ef20
Sandro Putraa <59155826+sandrocods@users.noreply.github.com>
2021-01-07 10:28:03 +0700

    Update run.php

--- run.php
+++ run.php
@@ -3,5 +3,14 @@
 require_once __DIR__ .'/lib/Class_tiktok.php';
 
 $download = New TiktokDownloader();
+
+
+//Download Single Video Using Link
 $analyze = $download->analyze('https://vt.tiktok.com/ZSvfNnV7/');
-print_r($download->Download($analyze['Data']['VideoID']));
+print_r($download->Download($analyze['Data']['VideoID'])); 
+
+/* Download Single Video Random by username
+Enter Username without @
+$a = $download->RandomVideo('saybillaaa_');
+$rand = $a['VideoID'][rand( 0 , count($a['VideoID']) -1)];
+print_r($rand); */
